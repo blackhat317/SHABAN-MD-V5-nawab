e74a056f58a4af2f6bb2912c3455454bbead1613
blackhat317 <ayeshakhanseo786@gmail.com>
2025-07-16 12:27:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~hBhk1JbQ#eEKDo4q3_346WE3_J03Xhh8ib4fOdVjXgCxdyIotoO0",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~FRwCkKDT#44lAIb8Nw88N9gvcnPsbh5YMjYNG02DI3hgUb_6WrOs",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
@@ -34,7 +34,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923046862036",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923098586776",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "THE LEGEND NAWAB",
 // add bot owner name
@@ -46,7 +46,7 @@ LIVE_MSG: process.env.LIVE_MSG || "> THE LEGEND NAWAB IS HERE GUY,S",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "true",
 // false or true for anti bad words  
